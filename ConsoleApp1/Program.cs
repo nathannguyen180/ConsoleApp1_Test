f32a78c4c26059cae665f017db0f8c58b9db97e3
Nathan Nguyen <nathannguyen180@gmail.com>
2019-07-25 15:44:20 -0400

    Basic Calculator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,59 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Suuuu Woop");
+          
+            for (; ; )
+            {
+                float num1 = 0; float num2 = 0;
+
+                Console.WriteLine("\n\n------------------------");
+                Console.WriteLine("Basic Console Calculator (in C#)\r");
+                Console.WriteLine("------------------------\n");
+
+                Console.WriteLine("Type a number, and then press Enter");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("Type another number, and then press Enter");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("Choose an option for the operation:");
+                Console.WriteLine("\t+ - Add");
+                Console.WriteLine("\t- - Substract");
+                Console.WriteLine("\t* - Multiply");
+                Console.WriteLine("\t/ - Divde");
+                Console.Write("Your Responds: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        Console.WriteLine($"Answer: {num1} + {num2} = " + (num1 + num2));
+                        break;
+                    case "-":
+                        Console.WriteLine($"Answer: {num1} - {num2} = " + (num1 - num2));
+                        break;
+                    case "*":
+                        Console.WriteLine($"Answer: {num1} * {num2} = " + (num1 * num2));
+                        break;
+                    case "/":
+                        // Ask the user to enter a non-zero divisor until they do so.
+                        while (num2 == 0)
+                        {
+                            Console.WriteLine("Enter a non-zero divisor: ");
+                            num2 = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                        break;
+
+                }
+                Console.WriteLine("Press any key to restart");
+                Console.ReadKey();
+                Console.Clear();
+                Console.Beep();
+             }
+            
         }
     }
 }
