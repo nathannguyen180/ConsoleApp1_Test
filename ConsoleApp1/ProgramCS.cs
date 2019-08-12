92778f4a371fe6800c513363b73d3405d2f69469
Nathan Nguyen <nathannguyen180@gmail.com>
2019-08-12 15:29:12 -0400

    blah

--- ConsoleApp1/ProgramCS.cs
+++ ConsoleApp1/ProgramCS.cs
@@ -81,12 +81,59 @@ namespace ConsoleApp1
             Dictionary<string, long> phonebook = new Dictionary<string, long>();
             phonebook.Add("Alex", 415434543);//
             phonebook["Jessica"] = 415984588;// both of these are methods of adding to the dictionary
+            phonebook.Add("sam", 415478943);
+            phonebook.Add("lmaobox", 415324543);
+            phonebook.Add("dan", 456148543);
 
             if (phonebook.ContainsKey("Alex"))
             {
+                Console.WriteLine("alec's number =" + phonebook["Alex"]);
 
             }
 
+            //the real benefit to using dictionary over list is the power to remove keys connected to numbers instead of the large numbers themselves
+            phonebook.Remove("Jessica");
+            Console.WriteLine(phonebook.Values);
+            Console.WriteLine();
+           /* for (int i = 0; i < phonebook.Count; i++)
+            {
+                Console.WriteLine(phonebook[i]);   //this is not possible
+            }*/
+            Console.WriteLine(phonebook.Count);
+            Console.ReadKey();
+
+
+            
+                Dictionary<string, int> inventory = new Dictionary<string, int>();
+                inventory["apple"] = 3;
+                inventory["orange"] = 5;
+                inventory["banana"] = 2;
+
+                Console.WriteLine(inventory["apple"]);
+                Console.WriteLine(inventory["orange"]);
+                Console.WriteLine(inventory["banana"]);
+            Console.ReadLine();
+
+            string emptyString = String.Empty; //read only? is this cringe?
+
+            int integer = 1;
+            string ourString = "Something to be replaced by an int"; //is completely replaced so now it only outputs "1"
+            ourString = integer.ToString();
+            Console.WriteLine(ourString);
+            Console.ReadLine();
+
+            int x = 2, y = 2;
+            int sum = x + y;
+            string sumCalculation = String.Format("{0} + {1} = {2}", x, y, sum);
+            Console.WriteLine(sumCalculation);
+            Console.ReadLine();
+
+            string fullString = "full string";
+            string someString = fullString.Substring(5);
+            string shorter = fullString.Substring(5, 3);
+            Console.WriteLine(someString);
+            Console.WriteLine(shorter);
+            Console.ReadLine();
         }
     }
 }
